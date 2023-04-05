7de77b91b4e204b2e25efcaa707178527fb7675a
Jennif6r <85253972+Jennif6r@users.noreply.github.com>
2023-04-05 18:24:05 +0200

    remove unsued imports

--- Server/src/main/java/services/Publisher.java
+++ Server/src/main/java/services/Publisher.java
@@ -1,11 +1,9 @@
 package main.java.services;
 
-import data.AllOrders;
 import jakarta.xml.ws.Endpoint;
 import main.java.services.impl.RegistratorImpl;
 import main.java.services.impl.RequestAktivOrderImpl;
 import main.java.services.impl.StartOrderImpl;
-import models.OrderList;
 
 public class Publisher {
 	public static void main(String[] args) {
--- Server/src/models/Product.java
+++ Server/src/models/Product.java
@@ -10,4 +10,28 @@ public class Product {
 		this.number = number;
 		this.price = price;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
 }
